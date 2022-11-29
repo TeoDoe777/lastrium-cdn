89b9e1ae78bbb871d6af98e30e959f3396eed6ea
Teo <turp777@yahoo.com>
2022-11-29 13:03:56 +0400

    Update custom-bg.js

--- custom-bg.js
+++ custom-bg.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   	if (location.href.match(/https?:\/\/(lastrium)(\.com)(\/files\/file\/)([0-9]*-skyrim-se-the-last-dark\/)/)) {
          $('body').addClass('page__skyrim-last-dark')
-        $( "<style>body.page__skyrim-last-dark:before {background: url('https://cdn.jsdelivr.net/gh/TeoDoe777/lastrium-cdn@1.0.0.1/lastrium-skyrim-last-dark-bg.png') no-repeat top center;}</style>" ).appendTo( "head" )
+        $( "<style>body.page__skyrim-last-dark:before {background: url('https://cdn.jsdelivr.net/gh/TeoDoe777/lastrium-cdn@1.0.0.1/lastrium-skyrim-last-dark-bg.png') no-repeat top center !important;}</style>" ).appendTo( "head" )
         }
 });

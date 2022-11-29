425f4ae7ea11d40922b24d77cbdc19e199548a8c
Teo <turp777@yahoo.com>
2022-11-29 12:36:23 +0400

    Create custom-bg.js

---
+++ custom-bg.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  if (location.href.match(/^(https?:\/\/(lastrium)(\.com)(\/files\/file\/)([0-9]*-skyrim-se-the-last-dark\/)) {
+        $('html').addClass('page__skyrim-last-dark');
+        $( "<style></style>" ).appendTo( "head" )
+    }
+});
